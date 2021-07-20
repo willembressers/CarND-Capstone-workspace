e4626a2706371136868e99f80d1f6b50dc07322a
Willem Bressers <dhr.bressers@gmail.com>
2021-07-20 15:21:18 +0000

    waypoint updater fix

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -44,7 +44,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(30)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
                 # Get closest waypoint
@@ -55,27 +55,26 @@ class WaypointUpdater(object):
     def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
-        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
         
-        # Check if closest is  ahead or behind vehicle
+        closest_idx = self.waypoint_tree.query([x,y],1)[1]
         closest_coord = self.waypoints_2d[closest_idx]
         prev_coord = self.waypoints_2d[closest_idx - 1]
+
+        # Equation for hyperplane through closest_coords
+        cl_vector = np.array(closest_coord)
+        prev_vector = np.array(prev_coord)
+        pos_vector = np.array([x,y])
         
-        # Equation for hyperplane through closest coords
-        cl_vect = np.array(closest_coord)
-        prev_vect = np.array(prev_coord)
-        pos_vect = np.array([x, y])
-        
-        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
-        
+        val = np.dot(cl_vector - prev_vector, pos_vector - cl_vector)
         if val > 0:
             closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        
         return closest_idx
     
     def publish_waypoints(self, closest_idx):
         lane = Lane()
         lane.header = self.base_waypoints.header
-        lane_waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
         self.final_waypoints_pub.publish(lane)
         
     def pose_cb(self, msg):
